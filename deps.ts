30a0e7b0e1c6b9a0ef363b897b6115052fb28f79
aapclark <aaronpclark92@gmail.com>
2020-07-24 16:29:33 -0500

    chore: add wip deps

---
+++ deps.ts
@@ -0,0 +1,9 @@
+// export { DataTypes, Database, Model } from "https://deno.land/x/denodb/mod.ts";
+export {
+  App,
+  Request,
+  Response,
+  logger,
+  parser,
+} from "https://deno.land/x/attain/mod.ts";
+// export { applyGraphQL, gql } from "https://deno.land/x/attain_graphql/mod.ts";
